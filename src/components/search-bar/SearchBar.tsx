7c21885164d80f297626d94e36706205eada626e
Agustín Chaves <aguchaves@MacBook-Air.local>
2021-02-20 19:53:10 -0300

    search-bar styles added

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import { ReduxProps } from '.';
 import { getItems } from '../../api';
 import debounce from 'lodash.debounce';
+import './search-bar.scss';
+import logo from '../../assets/Logo_ML@2x.png';
+import searchIcon from '../../assets/ic_Search@2x.png';
 
 interface Props extends ReduxProps {}
 
@@ -28,10 +31,14 @@ const SearchBar: React.FC<Props> = (props) => {
   }, [value])
 
   return (
-    <div>
-      <p html-for="search">{value}</p>
-      <input id="search" type="text" value={value} onChange={handleChange}></input>
-      <h1>{value}</h1>
+    <div className="search-bar">
+      <div className="search-bar__container">
+        <img className="search-bar__logo" src={logo} alt="ML-logo"/>
+        <input className="search-bar__input" id="search" type="text" placeholder="Nunca dejes de buscar" value={value} onChange={handleChange}></input>
+        <button className="search-bar__button">
+          <img src={searchIcon} alt="search"/>
+        </button>
+      </div>
     </div>
   )
 }
